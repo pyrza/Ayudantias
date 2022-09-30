62afa2b549585239ac6da36e75edd703583133c8
Igmacoi <106353397+Igmacoi@users.noreply.github.com>
2022-09-30 20:45:07 -0300

    funcion para el popup

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// JS para login (Registro e Inicio de Secion)
 const $btnSignIn= document.querySelector('.sign-in-btn'),
       $btnSignUp = document.querySelector('.sign-up-btn'),  
       $signUp = document.querySelector('.sign-up'),
@@ -8,4 +9,22 @@ document.addEventListener('click', e => {
         $signIn.classList.toggle('active');
         $signUp.classList.toggle('active')
     }
+});
+
+
+// JS para popup (Furmulario Practicas y Ayudantia)
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
 });
