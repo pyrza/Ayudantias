b912b31fd451433d1651c532ca3d525c1a627ee8
EduuJproo1 <103785387+EduuJproo1@users.noreply.github.com>
2022-10-26 02:01:19 -0300

    Entrega de Leo
    ayudantias

---
+++ Leonardo/Script.js
@@ -0,0 +1,31 @@
+// JS para login (Registro e Inicio de Secion)
+const $btnSignIn= document.querySelector('.sign-in-btn'),
+    $btnSignUp = document.querySelector('.sign-up-btn'),  
+    $signUp = document.querySelector('.sign-up'),
+    $signIn  = document.querySelector('.sign-in');
+
+document.addEventListener('click', e => {
+    if (e.target === $btnSignIn || e.target === $btnSignUp) {
+        $signIn.classList.toggle('active');
+        $signUp.classList.toggle('active')
+    }
+});
+
+
+// JS para popup (Formulario Prácticas y Ayudantía)
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+
