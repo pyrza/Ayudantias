ad88ccdf6b88d77f0c99102908fbaefe889a6cc2
Igmacoi <106353397+Igmacoi@users.noreply.github.com>
2023-04-14 19:05:45 -0400

    hola

--- Agustin/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $hostname = "db.inf.uct.cl";
-    $usuariodb = "A2022_ejaramillo";
-    $passworddb = "A2022_ejaramillo";
-    $dbname = "A2022_ejaramillo";
-    
-    
-?>
--- Agustin/taller de integra/recib_Delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('config.php');
-$idRegistros = $_REQUEST['id'];
-
-$DeleteRegistro = ("DELETE FROM trabajos WHERE id= '".$idRegistros."' ");
-mysqli_query($con, $DeleteRegistro);
-?>
